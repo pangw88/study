8d8be246fcdc46db6fe0f84699db7a2b66a663fe
wp <wp@192.168.0.102>
2019-02-07 19:48:57 +0800

    配置更新

--- study-swing/src/main/java/com/wp/study/swing/service/DBConfigService.java
+++ study-swing/src/main/java/com/wp/study/swing/service/DBConfigService.java
@@ -4,14 +4,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.wp.study.base.pojo.Config;
-import com.wp.study.base.util.IoUtils;
 import com.wp.study.jdbc.derby.dao.ConfigMapper;
 
 public class DBConfigService {
@@ -27,11 +24,9 @@ public class DBConfigService {
 	private static synchronized void initDBConfig() {
 		if(dbCache == null) {
 			dbCache = new HashMap<String, Object>();
-			String resource = "com/wp/study/swing/service/mybatis-config.xml";
 			SqlSession sqlSession = null;
 			try {
-				sqlSession = new SqlSessionFactoryBuilder().build(Resources  
-			        .getResourceAsReader(resource)).openSession();
+				sqlSession = SqlSessionService.getSqlSession();
 		        ConfigMapper configMapper = sqlSession.getMapper(ConfigMapper.class);
 		        List<Config> configs = configMapper.queryAllConfig();
 		        for(Config conf : configs) {
@@ -40,7 +35,7 @@ public class DBConfigService {
 	        } catch (Exception e) {  
 	        	LOG.error(e.getMessage());
 	        } finally {
-	        	IoUtils.closeQuietly(sqlSession);
+	        	SqlSessionService.commitAndClose(sqlSession);
 	        }
 		}
 	}
