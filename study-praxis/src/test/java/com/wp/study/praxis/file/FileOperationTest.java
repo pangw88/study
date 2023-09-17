a118086200edd112375ac0e2f2a2479f8620ddef
pangw88 <1820737934@qq.com>
2023-09-17 14:12:08 +0800

    新增配置

--- study-praxis/src/test/java/com/wp/study/praxis/file/FileOperationTest.java
+++ study-praxis/src/test/java/com/wp/study/praxis/file/FileOperationTest.java
@@ -37,8 +37,8 @@ public class FileOperationTest {
 //			String path = "E:\\DCIM\\temp";
 //			FileOperation.renameLivp2ZipAndUncompress(new File(path));
 
-			String path = "E:\\DCIM\\2023.part1";
-			FileOperation.replaceRename(new File(path), "VID_20230722_", "川西环线_0722_", ".jpg", false);
+			String path = "E:\\DCIM\\2018.part2_Rainbow计划.淘造物节.鲁班.塘栖";
+			FileOperation.replaceRename(new File(path), "LiveHouse_", "Rainbow计划_", ".jpg", false);
 //			FileOperation.replaceRename(new File(path), "2022-07-04 ", "成都outing_0704_", null);
 //			FileOperation.replaceRename(new File(path), "jpeg", "jpg", null);
 //			FileOperation.toJpg("E:\\DCIM\\IMG_1747.HEIC.heic", "E:\\DCIM\\IMG_1747.jpg");
